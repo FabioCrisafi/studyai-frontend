fa885c2aeee7af0e3546a6c2774de753f08ad1aa
FabioCrisafi <fabio.a.crisafi@gmail.com>
2026-06-23 12:41:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'studyai-v14';
+const CACHE = 'studyai-v15';
 const ASSETS = ['/', '/index.html', '/app.js', '/style.css', '/manifest.json'];
 
 self.addEventListener('install', e => {
